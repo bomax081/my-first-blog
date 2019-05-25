73249f8677361f7951239799fc525fe522d64dab
Sigrid <bo.max.081@gmail.com>
2019-05-25 14:10:41 +0200

    My Django Girls app. first commit

---
+++ scripts/loops.py
@@ -0,0 +1,12 @@
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monika', 'Phoebe', 'Ola', 'Sigrid']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for i in range(1, 6):
+    print(i)
+
